450e79fc2f1275c359b657a0c49a4587a46ab864
Thienhoangba99 <Thienhoangba99@users.noreply.github.com>
2019-11-30 08:49:32 +0700

    Add project files.

---
+++ Sky_TNT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sky_TNT.MvcApplication" Language="C#" %>
---
+++ Sky_TNT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sky_TNT.Startup))]
+namespace Sky_TNT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
